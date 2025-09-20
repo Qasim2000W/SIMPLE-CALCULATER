4616eebe08126a107d4667dd157118830dcfe8ea
Qasim2000W <q.a.s.i.m.2.m.o@gmail.com>
2025-09-20 03:35:21 +0300

    First commit - CALCULATER project

---
+++ CALCULATER/ClsMath.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static CALCULATER.Form1;
+
+namespace MATH
+{
+    public class ClsMath
+    {
+        public double? number1 = null;
+        public double? number2 = null;
+        public double? result = null;
+        public string type = string.Empty;
+        public string speartype = string.Empty;
+        public string StrMathMatical = string.Empty;
+
+        struct STtermmath
+        {
+            public double? number;
+            public string optype;
+        }
+
+        STtermmath[] ArrtermMath = new STtermmath[100];
+        STtermmath termmath = new STtermmath();
+
+        public enum ENSwitchnum
+        {
+            num1,
+            num2
+        }
+        public enum ENSwitchType
+        {
+            off,
+            on
+        }
+
+        public ENSwitchType switchType = ENSwitchType.off;
+        ENSwitchnum roundnum = ENSwitchnum.num1;
+
+
+
+        short count = 0;
+
+        public void FullArray()
+        {
+            ArrtermMath[count] = termmath;
+            count++;
+            termmath.number = null;
+            termmath.optype = string.Empty;
+        }
+
+
+        public void ProccessOP(string op)
+        {
+            if (string.IsNullOrEmpty(type))
+            {
+                type = op;
+            }
+            else
+            {
+                speartype = op;
+            }
+        }
+
+
+        public void ProcessKernel()
+        {
+           Calculate();
+           number1 = result;
+           number2 = null;
+           type = string.Empty;
+           ProccessOP(speartype);
+        }
+
+        public void ProccessInputs()
+        {
+            byte counter = 0;
+
+            while ((!string.IsNullOrEmpty(ArrtermMath[counter].optype)) || (ArrtermMath[counter].number != null))
+            {
+                ProccessOP(ArrtermMath[counter].optype);
+
+                switch (roundnum)
+                {
+                    case ENSwitchnum.num1:
+                        number1 = ArrtermMath[counter].number;
+                        roundnum = ENSwitchnum.num2;
+                        counter++;
+                        break;
+
+                    case ENSwitchnum.num2:
+                        number2 = ArrtermMath[counter].number;
+                        counter++;
+                        break;
+                }
+
+                ProcessKernel();
+            }
+        }
+
+        public void CleanVariableToContionusApp()
+        {
+            count = 0;
+            STtermmath[] NewArrtermMath = new STtermmath[100];
+            ArrtermMath = NewArrtermMath;
+            number2 = null;
+            result = null;
+        }
+
+        public bool ExistsOPIs()
+        {
+            return (this.StrMathMatical.Contains("+") || this.StrMathMatical.Contains("-") ||
+                        this.StrMathMatical.Contains("x") || this.StrMathMatical.Contains("/"));
+        }
+
+        public int FindFirstOP()
+        {
+            List<int> finds = new List<int>();
+
+            if (ExistsOPIs())
+            {
+                if (this.StrMathMatical.Contains("+"))
+                {
+                    finds.Add(this.StrMathMatical.IndexOf('+'));
+                }
+                if (this.StrMathMatical.Contains("-"))
+                {
+                    finds.Add(this.StrMathMatical.IndexOf('-'));
+                }
+                if (this.StrMathMatical.Contains("/"))
+                {
+                    finds.Add(this.StrMathMatical.IndexOf('/'));
+                }
+                if (this.StrMathMatical.Contains("x"))
+                {
+                    finds.Add(this.StrMathMatical.IndexOf('x'));
+                }
+                return finds.Min();
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public string SplitString(int index, int endindex)
+        {
+            string str = string.Empty;
+            string newmathmatical = string.Empty;
+            if (endindex <= 0)
+            {
+                str = this.StrMathMatical.Substring(0, this.StrMathMatical.Length);
+                this.StrMathMatical = "";
+            }
+            else
+            {
+                if (endindex == this.StrMathMatical.Length || this.StrMathMatical.Length == 0)
+                {
+                    str = this.StrMathMatical;
+                    this.StrMathMatical = "";
+                }
+                else
+                {
+                    str = this.StrMathMatical.Substring(index, endindex);
+                    newmathmatical = this.StrMathMatical.Substring(endindex, (this.StrMathMatical.Length - endindex));
+                    this.StrMathMatical = newmathmatical;
+                }
+            }
+            return str;
+        }
+
+
+        public void Calculate()
+        {
+            switch (type)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+
+                case "-":
+                    result = number1 - number2;
+                    break;
+
+                case "x":
+                    result = number1 * number2;
+                    break;
+
+                case "/":
+                    result = number1 / number2;
+                    break;
+            }
+        }
+    }
+}
---
+++ CALCULATER/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using MATH;
+using static MATH.ClsMath;
+
+
+namespace CALCULATER
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ClsMath math = new ClsMath();
+        string strnumber1 = string.Empty;
+        string strnumber2 = string.Empty;
+
+        public enum EnModeApp
+        {
+            startly,
+            continous
+        }
+
+        EnModeApp _Mode = EnModeApp.startly;
+
+        public void APPeariend(Button button)
+        {
+            textBox1.Text += button.Tag.ToString();
+        }
+
+       public void GetType(Button button)
+       {
+            if (math.switchType == ClsMath.ENSwitchType.on)
+            {
+                APPeariend(button);
+                math.switchType = ClsMath.ENSwitchType.off;
+            }
+            else
+            {
+                MessageBox.Show("the operation not avaliable", "wrong", MessageBoxButtons.OK);
+            }
+       }
+
+
+        public void GetUpDataToVariableClass()
+        {
+            if (math.number1 == null)
+            {
+                math.number1 = Convert.ToDouble(strnumber1);
+                math.number2 = Convert.ToDouble(strnumber2);
+            }
+            else
+            {
+                math.number2 = Convert.ToDouble(strnumber2);
+            }
+            strnumber1 = string.Empty;
+            strnumber2 = string.Empty;
+        }
+
+
+        private void FullStrNumber2InCaseOpEmpty()
+        {
+            int IndexOp = 0;
+
+            if (math.StrMathMatical.IndexOf("-") == 0 || math.StrMathMatical.IndexOf("+") == 0
+                       || math.StrMathMatical.IndexOf("/") == 0 || math.StrMathMatical.IndexOf("x") == 0)
+            {
+                math.type = math.SplitString(0, 1);
+                IndexOp = math.FindFirstOP();
+                strnumber2 = math.SplitString(0, IndexOp);
+            }
+            else
+            {
+                IndexOp = math.FindFirstOP();
+                strnumber2 = math.SplitString(0, IndexOp);
+                math.type = math.SplitString(0, 1);
+            }
+        }
+
+        private void FullStrNumber2()
+        {
+            int IndexOp = 0;
+
+            if (math.ExistsOPIs())
+            {
+                if (string.IsNullOrEmpty(math.type))
+                {
+                    FullStrNumber2InCaseOpEmpty();
+                }
+                else
+                {
+                    IndexOp = math.FindFirstOP();
+                    strnumber2 = math.SplitString(0, IndexOp);
+                }
+
+            }
+            else
+            {
+                strnumber2 = math.SplitString(0, math.StrMathMatical.Length);
+                math.StrMathMatical = "";
+            }
+        }
+
+        private void FullStrNumbersInFirstSub()
+        {
+            int IndexOp = 0;
+
+            math.type = math.SplitString(0, 1);
+
+            if (math.number1 == null)
+            {
+                IndexOp = math.FindFirstOP();
+                strnumber1 = math.SplitString(0, IndexOp);
+                FullStrNumber2();
+            }
+            else
+            {
+                FullStrNumber2();
+            }
+        }
+
+        private void FullStrNumbersInNormalMode()
+        {
+            int IndexOp = 0;
+
+            if (math.number1 == null)
+            {
+                IndexOp = math.FindFirstOP();
+                strnumber1 = math.SplitString(0, IndexOp);
+                math.type = math.SplitString(0, 1);
+                FullStrNumber2();
+            }
+            else
+            {
+                FullStrNumber2();
+            }
+        }
+
+        public void ConvertSTRMathToDecimal()
+        { 
+            while (!string.IsNullOrEmpty(math.StrMathMatical))
+            {
+                if (math.StrMathMatical.IndexOf("-")==0)
+                {
+                    FullStrNumbersInFirstSub();
+                }
+                else
+                {
+                    FullStrNumbersInNormalMode();
+                }
+
+                GetUpDataToVariableClass();
+                math.Calculate();
+                math.ProcessKernel();
+            }
+
+            textBox1.Text = math.result.ToString();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            APPeariend((Button)sender);
+            math.switchType = ClsMath.ENSwitchType.on;
+        }
+
+        
+        private void buttonOP_Click(object sender, EventArgs e)
+        {
+          GetType((Button)sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            math.StrMathMatical = textBox1.Text;
+            ConvertSTRMathToDecimal();
+            _Mode = EnModeApp.continous;
+
+            if (_Mode== EnModeApp.continous)
+            {
+                math.result = null;
+                math.number1 = null;
+                math.number2 = null;
+                strnumber1 = string.Empty;
+                strnumber1 = string.Empty;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            math.result = null;
+            math.number1 = null;
+            math.number2 = null;
+            strnumber1 = string.Empty;
+            strnumber1 = string.Empty;
+            math.speartype = string.Empty;
+            math.type = string.Empty;
+            _Mode =EnModeApp.startly;
+        }
+    }
+}
